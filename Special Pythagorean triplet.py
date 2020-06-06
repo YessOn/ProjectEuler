98a9e9dcdcc706a1aa4380f1c2015b8050343ebc
__Sniper__ <yess.mapss@gmail.com>
2020-06-06 21:32:03 +0100

    Create Special Pythagorean triplet.py

---
+++ Special Pythagorean triplet.py
@@ -0,0 +1,10 @@
+n = 1000
+for a in range(1, n):
+	for b in range(1, n):
+		for c in range(1, n):
+			if c**2 == a**2 + b**2 and a+b+c == 1000:
+				print(a, b, c)
+				print(a*b*c)
+print("Done!")
+
+# The answer: 31875000
